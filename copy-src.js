3f51eeae5bf2e5accdf65ba9d5b959b0935c69d8
thorcc <thorchrcow@gmail.com>
2021-12-18 13:30:05 +0100

    la til: beskjed til sensor

--- copy-src.js
+++ copy-src.js
@@ -6,3 +6,10 @@ fs.copy("./src","./LEVERING/Kode", err => {
         return console.error(err)
     }
 })
+
+fs.copy("./beskjed-til-sensor.txt", "./LEVERING/beskjed-til-sensor.txt", err => {
+    if(err){
+        console.log('An error occured while copying "beskjed-til-sensor.txt" to LEVERING.');
+        return console.error(err)
+    }
+})
